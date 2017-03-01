7102d30ee318bf15384afa20cd342e5a33aef3e1
Tang11jun <m15524529248@163.com>
2017-03-01 13:46:16 +0800

    add list remove functions

--- src/list.c
+++ src/list.c
@@ -28,7 +28,7 @@ int listAppend(List *list, void *value) {
     node->prev = list->tail;
     node->next = NULL;
     list->tail->next = node;
-    list->tail = node;
+    list->tail = node;//先把原来node所开辟出来的空间的next指向现在的node,相当于链表的链接功能。
     list->length++;
   }
   return 0;
@@ -46,6 +46,52 @@ Listnode *listIndexAt(List *list, long index) {
   return NULL;
 }
 
+int listRemoveAll(List *list) {
+  Listnode *current=list->head;
+  while(list->head!=NULL) {
+    current=current->next;
+    free(list->head);
+    list->head=current;
+    list->length--;
+  }
+  return 1;
+}
+
+int listRemoveAt(List *list, long index) {
+  Listnode *node, *pt,*pf;
+  if(list->length==0)
+    return (-1);
+  node = listIndexAt(list,index);
+  pt=node->prev;
+  pf=listIndexAt(list,index)->next;
+  if(list->destroy != NULL) {
+    list->destroy(listIndexAt(list,index)->value);
+  }
+  
+  if(node == list->head) {
+    
+    list->head = pf;
+  }
+  if(node == list->tail) {
+
+    free(listIndexAt(list,index));
+    list->length--;
+    list->tail=pt;
+  }
+  if(pt!=NULL && pf!=NULL) {
+    if(list->destroy != NULL) {
+      list->destroy(listIndexAt(list,index)->value);
+    }
+    free(listIndexAt(list,index));
+    pt->next=pf;
+    pt=pf;
+    list->length--;
+  }
+  list->length--;
+  free(node);
+  return 1;
+}
+
 // test main
 int main() {
   int i;
@@ -61,5 +107,12 @@ int main() {
     printf("%d\n", *(int *)(listIndexAt(&list, i)->value));
   }
 
+
+
+  listRemoveAt(&list,4);
+  listRemoveAt(&list,5);
+  printf("%d\n",list.length);
+  for(i=0; i<8; i++)
+    printf("%d\n", *(int *)(listIndexAt(&list, i)->value));
   return 0;
 }
