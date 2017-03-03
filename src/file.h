f32b09ebf80a6df5362d86bdd8d833ba47db8223
xjkdev <xjk2008@gmail.com>
2017-03-04 01:32:29 +0800

    add struct filehead

--- src/file.h
+++ src/file.h
@@ -3,6 +3,11 @@
 #include "list.h"
 #include "stData.h"
 
+typedef struct FileHead {
+  long length;
+  size_t width;
+} FileHead;
+
 int list_filesave(List *list, char *filename);
 int list_fileread(List *list, char *filename);
 int list_fileappend(List *list, char *filename, void *value);
