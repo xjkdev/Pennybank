209dd8094169dfb9f7c135f34071748fe7433459
MY1115 <1226350074@qq.com>
2017-03-04 21:42:19 +0800

    finish the lib.c

--- src/lib.h
+++ src/lib.h
@@ -3,4 +3,17 @@
 #include "list.h"
 #include "stData.h"
 
+typedef struct Check {
+    int year;
+    int month;
+    int earn;
+    int expense;
+}Check;
+int comparebytime(stData * stdata1,stData * stdata2);
+int comparebyid(stData * stdata1,stData * stdata2);
+int comparebyuserid(stData * stdata1,stData * stdata2);
+int comparebyamount(stData * stdata1,stData * stdata2);
+int comparebybalance(stData * stdata1,stData * stdata2);
+void check(List * list);
+
 #endif
