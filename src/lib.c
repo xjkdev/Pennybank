4a677e2fef7e2c3a08330673d38c69ac7348e1cd
Tang11jun <m15524529248@163.com>
2017-03-05 10:06:42 +0800

    finish the lib.h and lib.c

--- src/lib.c
+++ src/lib.c
@@ -1,1 +1,18 @@
 // this file is to add algorithm for search and sort and filter
+#include <stdio.h>
+#include <stdlib.h>
+#include <lib.h>
+#include <list.h>
+#include <stData.h>
+int  cBalance(List *list,long index){
+	Listnode *node;
+	node=listIndexAt(list,index);
+	Decimal n;
+	n=node->value->amount;
+	node=node->next;
+	while(node!=NULL){
+		node->value->balance-=n;
+		node=node->next;
+	}
+	return 0;  
+}
--- src/lib.h
+++ src/lib.h
@@ -2,5 +2,5 @@
 #define LIB_H
 #include "list.h"
 #include "stData.h"
-
+void cBalance(List *list,long index);
 #endif
